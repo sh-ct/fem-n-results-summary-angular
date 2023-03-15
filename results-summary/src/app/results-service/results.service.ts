e91962f641963a5362423de7a08c57c8f3b6014a
sh-ct <s1.turner@outlook.com>
2023-03-15 15:46:00 +0000

    results service getResults

--- results-summary/src/app/results-service/results.service.ts
+++ results-summary/src/app/results-service/results.service.ts
@@ -1,10 +1,17 @@
 import { Injectable } from '@angular/core';
+import * as jsonData from '../../assets/data/data.json'
 
 @Injectable({
   providedIn: 'root'
 })
 export class ResultsService {
 
+  results: any = jsonData;
+
   constructor() { }
+
+  getResults() {
+    return this.results;
+  }
 
 }
