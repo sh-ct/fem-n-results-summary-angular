e584a27cd9f33f30b077444e06ce15f2079f6389
sh-ct <s1.turner@outlook.com>
2023-03-15 15:47:03 +0000

    initial setup and results service

---
+++ results-summary/src/app/summary-item/summary-item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-summary-item',
+  templateUrl: './summary-item.component.html',
+  styleUrls: ['./summary-item.component.scss']
+})
+export class SummaryItemComponent {
+
+}
